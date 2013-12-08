f3bd8e93e078ab187b40b548cbcc26a6ccb548f6
unknown <narayanprabad@yahoo.co.in>
2013-12-09 01:17:33 +0530

    Twitter API Changes

---
+++ TwitterApp/src/com/twitter/tag/SubstrTagHandler.java
@@ -0,0 +1,57 @@
+package com.twitter.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class SubstrTagHandler extends TagSupport {
+	/**
+	 * Serialization.
+	 */
+	private static final long serialVersionUID = 5032443941347089105L;
+	private String input;
+	private int start;
+	private int end;
+
+	@Override
+	public int doStartTag() throws JspException {
+
+		try {
+			// Get the writer object for output.
+			JspWriter out = pageContext.getOut();
+
+			// Perform substr operation on string.
+			out.println(input.substring(start, end));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+}
